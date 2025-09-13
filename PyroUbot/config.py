19d4e25916fc10bef7af8ce73d552400d9406644
robingunawan <robingunawan75@gmail.com>
2025-09-14 06:33:16 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -19,7 +19,7 @@ BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002
 
 RMBG_API = os.getenv("RMBG_API", "KsPwpgY1Mncmr98ncrWpwZ22")
 
-MONGO_URL = os.getenv("MONGO_URL = "mongodb+srv://fatcatVbot:Medan2018@@cluster0.abcd123.mongodb.net/fatcatdb?retryWrites=true&w=majority")
+MONGO_URL = os.getenv("MONGO_URL = "mongodb+srv://fatcatVbot:Medan2018%40@cluster0.abcd123.mongodb.net/fatcatdb?retryWrites=true&w=majority")
 
 LOGS_MAKER_UBOT = os.getenv("LOGS_MAKER_UBOT", "-1003047431974")
 
